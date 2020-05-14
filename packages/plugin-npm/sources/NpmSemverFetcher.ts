4e17b6db628e6d331fdd1b6d065d4bc04b2e2549
Michael Orenstein <michael@whiteshadows.me>
2020-05-14 14:56:48 +0930

    chore: More warning silencing

--- packages/plugin-npm/sources/NpmSemverFetcher.ts
+++ packages/plugin-npm/sources/NpmSemverFetcher.ts
@@ -32,7 +32,7 @@ export class NpmSemverFetcher implements Fetcher {
 
     const [packageFs, releaseFs, checksum] = await opts.cache.fetchPackageFromCache(locator, expectedChecksum, {
       onHit: () => opts.report.reportCacheHit(locator),
-      onMiss: () => opts.report.reportCacheMiss(locator, `${structUtils.prettyLocator(opts.project.configuration, locator)} can't be found in the cache and will be fetched from the remote registry`),
+      onMiss: () => opts.report.reportCacheMiss(locator),
       loader: () => this.fetchFromNetwork(locator, opts),
     });
 
