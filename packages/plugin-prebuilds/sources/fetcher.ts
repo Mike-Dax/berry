d7925f20732757d1d1cc666cfa870cbc82aef9f7
Michael Orenstein <michael@whiteshadows.me>
2020-06-02 12:22:41 +0930

    chore: Don't cache prebuild fetches

--- packages/plugin-prebuilds/sources/fetcher.ts
+++ packages/plugin-prebuilds/sources/fetcher.ts
@@ -1,8 +1,8 @@
-import {Fetcher, structUtils, FetchOptions, MinimalFetchOptions, miscUtils, FetchResult, ReportError, MessageName, Locator} from '@yarnpkg/core';
-import {ppath, xfs, ZipFS, Filename, CwdFS, PortablePath}                                                                   from '@yarnpkg/fslib';
-import {getLibzipPromise}                                                                                                   from '@yarnpkg/libzip';
+import {Fetcher, structUtils, FetchOptions, MinimalFetchOptions, miscUtils, FetchResult, ReportError, MessageName, Locator, hashUtils} from '@yarnpkg/core';
+import {ppath, xfs, ZipFS, Filename, CwdFS, PortablePath, LazyFS, NodeFS}                                                              from '@yarnpkg/fslib';
+import {getLibzipPromise}                                                                                                              from '@yarnpkg/libzip';
 
-import * as utils                                                                                                           from './utils';
+import * as utils                                                                                                                      from './utils';
 
 
 export class PrebuildFetcher implements Fetcher {
@@ -18,23 +18,43 @@ export class PrebuildFetcher implements Fetcher {
   }
 
   async fetch(locator: Locator, opts: FetchOptions) {
-    const expectedChecksum = null; // opts.checksums.get(locator.locatorHash) || null;
+    const baseFs = new NodeFS();
 
-    const [packageFs, releaseFs, checksum] = await opts.cache.fetchPackageFromCache(
-      locator,
-      expectedChecksum,
-      {
-        onHit: () => opts.report.reportCacheHit(locator),
-        onMiss: () => opts.report.reportCacheMiss(locator),
-        loader: () => {
-          // opts.report.reportInfoOnce(MessageName.FETCH_NOT_CACHED, `${structUtils.prettyLocator(opts.project.configuration, locator)} can't be found in the cache and will be fetched from the registry`);
-          return this.fetchPrebuild(locator, opts);
-        },
+    const zipFs = await this.fetchPrebuild(locator, opts);
+    const originalPath = zipFs.getRealPath();
+
+    zipFs.saveAndClose();
+
+    await xfs.chmodPromise(originalPath, 0o644);
+
+    // Do this before moving the file so that we don't pollute the cache with corrupted archives
+    const checksum = `${opts.cache.cacheKey}/${await hashUtils.checksumFile(originalPath)}`;
+
+    const cachePath = opts.cache.getLocatorPath(locator, checksum);
+
+    if (!cachePath)
+      throw new Error(`Assertion failed: Expected the cache path to be available`);
+
+    await xfs.movePromise(originalPath, cachePath);
+    await xfs.mkdirpPromise(ppath.dirname(cachePath));
+
+    let readOnlyZipFs: ZipFS | null = null;
+
+    const libzip = await getLibzipPromise();
+    const lazyFs: LazyFS<PortablePath> = new LazyFS<PortablePath>(() => miscUtils.prettifySyncErrors(() => {
+      return readOnlyZipFs = new ZipFS(cachePath, {baseFs, libzip, readOnly: true});
+    }, message => {
+      return `Failed to open the cache entry for ${structUtils.prettyLocator(opts.project.configuration, locator)}: ${message}`;
+    }), ppath);
+
+    const releaseFs = () => {
+      if (readOnlyZipFs !== null) {
+        readOnlyZipFs.discardAndClose();
       }
-    );
+    };
 
     return {
-      packageFs,
+      packageFs: lazyFs,
       releaseFs,
       prefixPath: structUtils.getIdentVendorPath(locator),
       localPath: this.getLocalPath(locator, opts),
@@ -45,8 +65,9 @@ export class PrebuildFetcher implements Fetcher {
   private async fetchPrebuild(locator: Locator, opts: FetchOptions) {
     const {packageIdent} = utils.parseSpec(locator.reference);
 
-    const electronVersion = await utils.getElectronVersion(opts.project);
+    // opts.report.reportInfo(MessageName.UNNAMED, `Fetching prebuild for ${structUtils.stringifyIdent(locator)}`);
 
+    const electronVersion = await utils.getElectronVersion(opts.project);
     const nativeModule = await utils.getNativeModule(opts.project, packageIdent, locator);
 
     if (nativeModule === null)
@@ -66,13 +87,27 @@ export class PrebuildFetcher implements Fetcher {
 
     let prebuildPackage: FetchResult;
     try {
-      prebuildPackage = await opts.fetcher.fetch(structUtils.makeLocator(structUtils.makeIdent(`prebuilds`, `${structUtils.slugifyIdent(nativeModule)}-v${nativeModule.version}-${process.platform}-${process.arch}-${prebuildOptions.runtime}-${prebuildOptions.abi}`), prebuildUrl), opts);
+      prebuildPackage = await opts.fetcher.fetch(
+        structUtils.makeLocator(
+          structUtils.makeIdent(
+            `prebuilds`,
+            `${structUtils.slugifyIdent(nativeModule)}-v${
+              nativeModule.version
+            }-${
+              process.platform
+            }-${
+              process.arch
+            }-${
+              prebuildOptions.runtime
+            }-${
+              prebuildOptions.abi
+            }`), prebuildUrl), opts);
     } catch (e) {
       opts.report.reportInfo(MessageName.UNNAMED, `Error fetching ${prebuildUrl}`);
       throw e;
     }
 
-    // opts.report.reportInfo(MessageName.UNNAMED, `Fetched prebuild for ${structUtils.stringifyIdent(nativeModule)} version ${nativeModule.version} on runtime electron version ${electronVersion}`)
+    // opts.report.reportInfo(MessageName.UNNAMED, `Fetched prebuild for ${structUtils.stringifyIdent(nativeModule)} version ${nativeModule.version} on runtime electron version ${electronVersion}`);
 
     const cancellationSignal = {cancel: false};
     let nodeContents: Buffer | null = null;
@@ -91,7 +126,6 @@ export class PrebuildFetcher implements Fetcher {
 
     if (nodeContents === null)
       throw new ReportError(MessageName.UNNAMED, `Was unable to find node file in prebuild package for "${structUtils.stringifyIdent(nativeModule)}"`);
-
 
     const tmpDir = await xfs.mktempPromise();
     const tmpFile = ppath.join(tmpDir, `prebuilt.zip` as Filename);
@@ -123,6 +157,7 @@ module.exports = (fileLookingFor) => {
 
     // Write the file into the generated package
     await generatedPackage.writeFilePromise(`bindings.node` as Filename, nodeContents);
+
 
     return zipPackage;
   }
