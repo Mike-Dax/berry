11e46f0ca7b816aab9919385bcb18fef3a0b4801
Michael Orenstein <michael@whiteshadows.me>
2020-06-02 12:22:19 +0930

    chore: Add special case for fsevents

--- packages/plugin-prebuilds/sources/add-prebuilt-dependencies.ts
+++ packages/plugin-prebuilds/sources/add-prebuilt-dependencies.ts
@@ -1,19 +1,22 @@
-import {Descriptor, Project, Locator, structUtils} from '@yarnpkg/core';
+import {Descriptor, Project, Package, structUtils} from '@yarnpkg/core';
 
 
 export const reduceDependency = async (
   dependency: Descriptor,
   project: Project,
-  locator: Locator,
+  pkg: Package,
 ) => {
+  // Temporary special case for fsevents on platforms that aren't darwin.
+  if (pkg.name === `fsevents` && process.platform !== `darwin`)
+    return dependency;
+
   if (dependency.name === `bindings` && dependency.scope === null) {
     // extra.resolveOptions.report.reportInfo(MessageName.UNNAMED, `Found a bindings dependency in ${structUtils.stringifyIdent(locator)}, re-routing to prebuild.`);
-
-    const selector = `builtin<prebuild/${structUtils.stringifyIdent(locator)}>`; // TODO: Add process.platform and arch to this
+    const selector = `builtin<prebuild/${structUtils.stringifyIdent(pkg)}>`; // TODO: Add process.platform and arch to this
 
     return structUtils.makeDescriptor(dependency, structUtils.makeRange({
       protocol: `prebuild:`,
-      source: `bindings<${structUtils.slugifyIdent(locator)}>${process.platform}-${process.arch}`,
+      source: `bindings<${structUtils.slugifyIdent(pkg)}>${process.platform}-${process.arch}`,
       selector,
       params: null,
     }));
